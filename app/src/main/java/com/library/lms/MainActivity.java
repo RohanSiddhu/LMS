d68096cb91894941e275263cb97436a5215e19f5
Rohan Siddhu <rohansiddhu21@gmail.com>
2022-07-07 15:20:31 +0530

    Added simple login screen

--- app/src/main/java/com/library/lms/MainActivity.java
+++ app/src/main/java/com/library/lms/MainActivity.java
@@ -3,12 +3,23 @@ package com.library.lms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void adminLogin(View view) {
+        Log.d(LOG_TAG, "Admin Login");
+    }
+
+    public void userLogin(View view) {
+        Log.d(LOG_TAG, "User Login");
     }
 }
