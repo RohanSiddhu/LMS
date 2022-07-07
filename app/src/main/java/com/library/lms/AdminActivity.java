b45b3e363006ff39046e36fcfeb35ab893ebce00
Rohan Siddhu <rohansiddhu21@gmail.com>
2022-07-07 21:18:45 +0530

    Added custom Toolbar to AdminActivity

--- app/src/main/java/com/library/lms/AdminActivity.java
+++ app/src/main/java/com/library/lms/AdminActivity.java
@@ -1,6 +1,7 @@
 package com.library.lms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
     }
 }
