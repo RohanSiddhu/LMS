0d2f072fd334f8d7e73388c2ced585047f52e660
Rohan Siddhu <rohansiddhu21@gmail.com>
2022-07-10 08:38:20 +0530

    Added ListView to display books.

--- app/src/main/java/com/library/lms/BooksActivity.java
+++ app/src/main/java/com/library/lms/BooksActivity.java
@@ -3,12 +3,35 @@ package com.library.lms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import com.library.lms.Adapter.*;
+
 
 public class BooksActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
+
+        listView = findViewById(R.id.listView_books);
+
+        ArrayList<String> bookList = new ArrayList<>();
+
+        bookList.add("Book Name");
+        bookList.add("Book Name");
+        bookList.add("Book Name");
+        bookList.add("Book Name");
+        bookList.add("Book Name");
+        bookList.add("Book Name");
+
+        BookAdapter adapter = new BookAdapter(this, bookList);
+
+        listView.setAdapter(adapter);
     }
 }
